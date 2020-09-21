b5be2e05f35667e9a64fe27e43fafbedc5197205
Gilmar Lima de Souza <gilmar.limasou@gmail.com>
2020-09-21 17:59:56 -0300

    TypeScript parte 2: Mais técnicas e boas práticas (Aula 02 - Lapidando nosso código)

--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -7,7 +7,7 @@ export class NegociacaoController{
     private _inputQuantidade: JQuery;
     private _inputValor: JQuery;
     private _negociacoes = new Negociacoes();
-    private _negociacoesView = new NegociacoesView('#negociacoesView');
+    private _negociacoesView = new NegociacoesView('#negociacoesView', true);
     private _mensagemView = new MensagemView('#mensagemView');
 
     constructor(){
@@ -22,8 +22,15 @@ export class NegociacaoController{
 
         event.preventDefault();
 
+        let data = new Date(this._inputData.val().replace(/-/g, ','));
+
+        if (!this._ehDiaUtil(data)){
+            this._mensagemView.update('Somente negociações em dias úteis, por favor!');
+            return;
+        }
+
         const negociacao = new Negociacao(
-            new Date(this._inputData.val().replace(/-/g, ',')),
+            data,
             parseInt(this._inputQuantidade.val()),
             parseFloat(this._inputValor.val())
         )
@@ -32,4 +39,20 @@ export class NegociacaoController{
         this._negociacoesView.update(this._negociacoes);
         this._mensagemView.update('Negociação adicionada com sucesso!');        
     }
+
+    private _ehDiaUtil(data: Date){
+
+        return data.getDay() != DiaDaSemana.Domingo && data.getDay() != DiaDaSemana.Sabado
+    }
 } 
+
+enum DiaDaSemana {
+
+    Domingo,
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado
+}
