8b8bb27ab9cca26d4a0295ac3a3974a376f7512f
Gilmar Lima de Souza <gilmar.limasou@gmail.com>
2020-09-22 10:53:36 -0300

    TypeScript parte 2: Mais técnicas e boas práticas (Aula 03 - O poder dos decorators)

--- alurabank/app/ts/views/View.ts
+++ alurabank/app/ts/views/View.ts
@@ -1,3 +1,5 @@
+import { logarTempoDeExecucao } from '../helpers/decorators/index';
+
 export abstract class View<T> {
     
     private _elemento: JQuery;
@@ -9,7 +11,9 @@ export abstract class View<T> {
         this._escapar = escapar;
     }
 
+    @logarTempoDeExecucao()
     update(model: T){
+
         let template = this.template(model);
 
         if (this._escapar)
